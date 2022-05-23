12c4161b09eb4cd4ea54edc5c843823fa4bfe05d
Radek Stibora <rstibora@gmail.com>
2022-05-23 14:36:29 +0200

    Add WIP second star solution for day 4

--- AdventOfCode2020/Day04.cs
+++ AdventOfCode2020/Day04.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode2020
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2020
 {
     internal class Day04 : IDay
     {
@@ -15,16 +17,49 @@
                 (string passportString) => passportString.Contains("pid:"),
 
             };
-            int validPassportsCount = 0;
-            for (int i = 0; i < passportStrings.Length; i++)
-                if (validityTests.All(test => test(passportStrings[i])))
-                    validPassportsCount++;
-            return validPassportsCount.ToString();
+            return countValidPassports(passportStrings, validityTests).ToString();
         }
 
         public string SecondStar(string[] inputLines)
         {
-            throw new NotImplementedException();
+            Func<string, bool> ruleWrapper(string fieldName, Func<string, bool> detailedCheck)
+            {
+                return (string passportString) =>
+                {
+                    if (!passportString.Contains(fieldName))
+                        return false;
+                    return detailedCheck(passportString.Split(fieldName)[1].Split(' ', '\n')[0]);
+                };
+            }
+
+            var passportStrings = string.Join("\n", inputLines).Split("\n\n");
+            var validityTests = new[] {
+                ruleWrapper("byr:", dataFragment => dataFragment.Length == 4 && Int32.Parse(dataFragment) >= 1920 && Int32.Parse(dataFragment) <= 2002),
+                ruleWrapper("iyr:", dataFragment => dataFragment.Length == 4 && Int32.Parse(dataFragment) >= 2010 && Int32.Parse(dataFragment) <= 2020),
+                ruleWrapper("eyr:", dataFragment => dataFragment.Length == 4 && Int32.Parse(dataFragment) >= 2020 && Int32.Parse(dataFragment) <= 2030),
+                ruleWrapper("hgt:", dataFragment =>
+                {
+                    return dataFragment[^2..] switch {
+                        "cm" => Int32.Parse(dataFragment[..^2]) >= 150 && Int32.Parse(dataFragment[..^2]) <= 193,
+                        "in" => Int32.Parse(dataFragment[..^2]) >= 59 && Int32.Parse(dataFragment[..^2]) <= 76,
+                        _ => false
+                    };
+                }),
+                ruleWrapper("hcl:", dataFragment => new Regex(@"#[0-9a-f]{6,6}").Matches(dataFragment).Count == 1),
+                ruleWrapper("ecl:", dataFragment => (new []{"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}).Contains(dataFragment)),
+                ruleWrapper("pid:", dataFragment => new Regex(@"[0-9]{9,9}").Matches(dataFragment).Count == 1),
+            };
+            return countValidPassports(passportStrings, validityTests).ToString();
+        }
+
+        private int countValidPassports(string[] passportStrings, Func<string, bool>[] validityTests)
+        {
+            foreach (string strr in (passportStrings.Where(passportString => validityTests.All(test => test(passportString)))))
+            {
+                System.Console.WriteLine(strr);
+                System.Console.Write("---\n");
+            };
+            return passportStrings.Where(passportString => validityTests.All(test => test(passportString))).Count();
         }
     }
 }
