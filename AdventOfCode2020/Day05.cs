f5736588b38d74d0c95c5d18beea2f4b59bce8d9
Radek Stibora <rstibora@gmail.com>
2022-06-07 08:16:16 +0200

    Add first star solution for day 5

---
+++ AdventOfCode2020/Day05.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+
+namespace AdventOfCode2020;
+
+internal class Day05 : IDay
+{
+    private int boardingPassId(string boardingPassString)
+    {
+        var columnBitArray = new BitArray(boardingPassString[7..].Select(character => character == 'R').Reverse().ToArray());
+        var rowBitArray = new BitArray(boardingPassString[..7].Select(character => character == 'B').Reverse().ToArray());
+
+        int[] array = new int[1];
+        columnBitArray.CopyTo(array, 0);
+        var column = array[0];
+
+        rowBitArray.CopyTo(array, 0);
+        var row = array[0];
+        return row * 8 + column;
+    }
+    public string FirstStar(string[] inputLines)
+    {
+        return inputLines.Select(boardingPassId).Max().ToString();
+    }
+
+    public string SecondStar(string[] inputLines)
+    {
+        throw new NotImplementedException();
+    }
+}
--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -1,8 +1,8 @@
 ﻿using AdventOfCode2020;
 
-string[] inputLines = System.IO.File.ReadAllLines($"{AppDomain.CurrentDomain.BaseDirectory}/InputDay04.txt");
+string[] inputLines = System.IO.File.ReadAllLines($"{AppDomain.CurrentDomain.BaseDirectory}/InputDay05.txt");
 
-var day = new Day04();
+var day = new Day05();
 
 var firstStarSolution = "not implemented";
 var secondStarSolution = "not implemented";
